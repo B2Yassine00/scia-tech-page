8c2c2c5041bc74e8d0c4ff547640cc9ac6a389f8
B2Yassine00 <banouq90@gmail.com>
2023-10-17 17:55:18 +0100

    about update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,34 +6,35 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 import { useState } from "react";
 import { about, slides } from "../constants";
+import { EarthCanvas } from "./canvas";
 
 const About = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
 
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
 
-  return (
-    <>
-      <div className="h-screen pt-0">
-        <div className="flex flex-col lg:grid gap-12 lg:grid-cols-2">
-          <div className="">
-            <motion.div variants={textVariant()}>
-              <h2 className={styles.sectionHeadText}>{about.big_title}</h2>
-            </motion.div>
+    return (
+        <>
+            <div className="h-screen pt-0">
+                <div className="flex flex-col lg:grid gap-12 lg:grid-cols-2">
+                    <div className="">
+                        <motion.div variants={textVariant()}>
+                            <h2 className={styles.sectionHeadText}>{about.big_title}</h2>
+                        </motion.div>
 
                         <motion.p
                             variants={fadeIn("", "", 0.1, 1)}
@@ -41,17 +42,17 @@ const About = () => {
                             {about.description}
                         </motion.p>
                     </div>
-                    <div className="hidden lg:flex w-[100%]">
-                        <div className=' max-w-[1000px] min-h-[400px] h-full w-[100%] -mt-10 m-auto px-20 relative group'>
+                    <div className="hidden lg:flex w-[100%] h-[540px]">
+                        <EarthCanvas />
+                        {/*<div className=' max-w-[1000px] min-h-[400px] h-full w-[100%] -mt-10 m-auto px-20 relative group'>
                             <div
                                 style={{ backgroundImage: `url(${slides[currentIndex].url}` }}
                                 className='w-full h-full rounded-2xl bg-center bg-cover bg-no-repeat duration-500'
                             ></div>
-                            {/* Left Arrow */}
+                            
                             <div className='group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-[#05668D] text-white cursor-pointer'>
                                 <BsChevronCompactLeft onClick={prevSlide} size={30} />
                             </div>
-                            {/* Right Arrow */}
 
                             <div className='group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-[#05668D] text-white cursor-pointer'>
                                 <BsChevronCompactRight onClick={nextSlide} size={30} />
@@ -68,7 +69,7 @@ const About = () => {
                                     </div>
                                 ))}
                             </div>
-                        </div>
+                        </div>*/}
                     </div>
                 </div>
             </div>
